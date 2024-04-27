5612c8a6333e336dd555825b7e37a299f407cba9
Ayan Pakhira <pakhiraayan@gmail.com>
2024-04-27 18:55:40 +0530

    created  app

---
+++ app/components/nav/BackDrop.tsx
@@ -0,0 +1,14 @@
+interface BackdropProps {
+  onClick: () => void;
+}
+
+const BackDrop: React.FC<BackdropProps> = ({ onClick }) => {
+  return (
+    <div
+      onClick={onClick}
+      className="z-20 bg-slate-200 opacity-50 w-screen h-screen fixed top-0 left-0"
+    ></div>
+  );
+};
+
+export default BackDrop;
---
+++ libs/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDd2tBeI29xD8ejTxVgh96OBySiD1XZ3d8",
+  authDomain: "e-shop-71e08.firebaseapp.com",
+  projectId: "e-shop-71e08",
+  storageBucket: "e-shop-71e08.appspot.com",
+  messagingSenderId: "27667443977",
+  appId: "1:27667443977:web:dc9b7ddab76c8cc947e3d8",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
